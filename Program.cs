c68f3f9b941f64920d4f8a9b99cb19164dcf41f4
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-09 13:39:22 +0100

    final changes

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ lookup["animal"] = "Not a human";
 lookup["fish"] = "No a human that swims";
 lookup["human"] = "human";
 
+Console.WriteLine($"The defintion of fish is {lookup["fish"]}");
+
+
+//
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[95] = "Tim Corey";
+employees[28] = "Sue Storm";
+
+Console.WriteLine($"The employee with ID number 28 is {employees[28]}");
