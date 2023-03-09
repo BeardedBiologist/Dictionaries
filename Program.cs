1d78ecdd1007e0b599d1da9d1b40b8789f922de9
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-09 13:35:13 +0100

    wrote code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿
 
 
+Dictionary<string, string> lookup = new Dictionary<string, string>();
 
+lookup["animal"] = "Not a human";
+lookup["fish"] = "No a human that swims";
+lookup["human"] = "human";
 
